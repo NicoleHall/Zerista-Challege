97a45246ddfee7832d03bbcaee6e96358764de5f
Nicole Hall <nicolealexandrahall@gmail.com>
2016-04-18 12:08:23 -0600

    had to comment out fixtures because I couldn't override the ID's and the fixtures were generating random ID's for tracks which I did not want. I added three test Event objects and three test Track Objects to the test helper. Tested that relationship is set up correctly and test pass

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "event holds a reference to the track id from the track table" do
+    assert_equal 3, Event.last.track_id
+    assert_equal "cross-session", Track.find(Event.last.track_id).session
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,53 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/pride'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  def setup
+    create_three_track_objects
+    create_three_event_objects
+    add_track_ids_to_event_objects
+  end
+
+  def create_three_track_objects
+    Track.create(session: "1")
+    Track.create(session: "2")
+    Track.create(session: "cross-session")
+  end
+
+  def create_three_event_objects
+    Event.create(
+      start: "12/8/2015 8:00",
+      finish: "12/8/2015 9:50",
+      name: "T3P4OR3 Segmentation and Registration",
+      track: "Track 1 - Pattern Recognition"
+    )
+
+    Event.create(
+      start: "12/8/2015 8:00",
+      finish: "12/8/2015 8:45",
+      name: "Image Computing for Digital Pathology",
+      track: "Track 2 - Image, Speech, and Signal Processing"
+    )
+
+    Event.create(
+      start: "12/8/2015 7:00",
+      finish: "12/8/2015 7:45",
+      name: "Monday Plenary Session 1",
+      track: "Keynote"
+    )
+
+    def add_track_ids_to_event_objects
+      Event.first.update(track_id: 1)
+      Event.second.update(track_id: 2)
+      Event.last.update(track_id: 3)
+    end
+  end
+
 end
